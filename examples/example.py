7fec1da2a6dcc01ded10a71e03b5abb44df2fa84
Pavel <pavel@enablebanking.com>
2023-09-20 14:13:07 +0300

    add signature example

--- examples/example.py
+++ examples/example.py
@@ -7,7 +7,7 @@ from urllib.error import HTTPError
 # requests used as an example for sending request. You can safely use urllib
 import requests
 
-HOST = "https://localhost"
+HOST = "http://localhost"
 BROKER_CLIENT_CERT_PATH = "broker_client_tls/client.crt"
 BROKER_CLIENT_KEY_PATH = "broker_client_tls/client.key"
 BROKER_CA_CERT_PATH = "broker_client_tls/ca.crt"
@@ -23,7 +23,7 @@ def sign() -> str:
         json={
             "params": {
                 "data": "test",
-                "key_id": "seal.key",
+                "key_id": "example.key",
                 "crypto_algorithm": "PS",
             },
         },
@@ -70,6 +70,6 @@ def make_request_urllib() -> Dict[Any, Any]:
 
 if __name__ == "__main__":
     # You can uncomment these functions one by one and check their responses
-    response = sign()
+    response = sign() # response: K8ANFONiZkGjde7JarIT0zIbo7FuA5YgRLetx8e+N+Ed00V9P1QicHvssHYvJ+zH3NWGBYwY+v0UOJjSJJSP2E+WWOAqD3W8ad61H+FlLChfkY3nsm/PNtGPR/9xtYvLltXels8fLOTmXEOnPzW+RsKxTnAL1MFTjlgkYpweTagtpCwWFZ3qdiz9FXp4+7qsUj+5bwtKV1m9ncM9NOvVGdFzTOpqLjqgeWyCLAcU+t7t8YwtxNuYaOox1STphdeVc96bLRcJnP1gHOnWqxlSnjgoc/rlscsZ/wIw7xpB+dCjmhGAeGZQlSLanH108f7x/WaaDiQxLmgaXsAWtcQoS6j6QNPN3v5+4rQfUJ0Vc6ktB9uJhbSjh/h0eEIYEylXY5da6nnE+mnLBCHcVaZSDOSfU86Zy8/2qUtDSHhwz4XuqzK2RaTXWSxy/OQhQH9ZORgvnlHchpJcx5xg1CgS752RV7NKRThfGNu9Nn6lSMEOWV8TbnLRBbeEx7jdhV3DRKGoYzWs0swHppB48xj+A2kG/zVspa8wbcpWIu5ZxtevSjMofcZ4d22SF3CQXKWpWOsR/WVINl2dtSDFq3NfqmbzItEEHr3j7s6y0gG4ERmSsez2uC/Fx9BxxS25QWQ1PctOn28uiHGSQ5TyhKimWW1pDRoagEiZYxnM9nshCpA=
     # response = make_request_urllib()
     print(response)
